6491f7b3b544039d3d9fb888533500b5a478a54b
shoma-mano <ms2geki@gmail.com>
2021-05-30 10:47:39 +0900

    first

---
+++ src/main/java/com/example/demo/mapper/HeroMapper.java
@@ -0,0 +1,27 @@
+package com.example.demo.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.model.Hero;
+
+@Mapper
+public interface HeroMapper {
+	
+void insertHero(Hero hero);
+
+public List<Hero> getMarvelHeroes();
+
+public List<Hero> getHeroes();
+
+public List<Hero> getHeroesfromemail(String email);
+
+public Hero getHero(int id);
+
+void deleteHero(int id);
+
+void updateHero(Hero hero);
+
+public List<Hero> searchHero(String term);
+}
---
+++ src/main/java/com/example/model/Hero.java
@@ -0,0 +1,12 @@
+package com.example.model;
+import com.example.model.Hero;
+import lombok.Data;
+
+@Data
+public class Hero {
+
+	private int id;
+	private String url;
+	private String name;
+	private String email;
+}
